9317cd2bf9e1564b7b48bde3f795ba49b8102be5
Farhan Abdilah Ramadhan <akunganda787@gmail.com>
2025-08-29 11:20:09 +0700

    memperbaiki route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 
-// Redirect root langsung ke projects.index
-Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
+// Root diarahkan ke tasks.index
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 
+// Resource
 Route::resource('projects', ProjectController::class);
 Route::resource('tasks', TaskController::class);
+
 
 
 Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
